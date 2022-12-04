2b4411fbda777282f123722fb93bc8ef300954f6
496114 <imgsrc366@gmail.com>
2022-12-04 20:34:34 +0900

    vueの設定を修正、ログアウトメソッドの調整

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,6 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '@fortawesome/fontawesome-free/js/all';
-import Vue from 'vue/dist/vue.esm'
 import TurbolinksAdapter from 'vue-turbolinks'
 
 Vue.use(TurbolinksAdapter)
